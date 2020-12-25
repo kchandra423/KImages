300e747d2025372004491a20fe8a7fddba66df0e
KumarSChandra <58375738+KumarSChandra@users.noreply.github.com>
2020-12-25 12:47:11 -0800

    gradle is confusing

---
+++ Textures/src/main/java/Textures/PGif.java
@@ -0,0 +1,137 @@
+/*
+MIT License
+
+Copyright (c) 2020 Kumar.s.Chandra
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package Textures;
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import destiny.gif.GifDecoder;
+import destiny.gif.GifDecoder.GifImage;
+import processing.core.PApplet;
+import processing.core.PImage;
+class PGif extends Texture{
+	private Fader fader;
+	private Frame[] frames;
+	private int curFrame;
+	private long lastTime;
+	PGif(String pathname) {
+		super(0,0);
+		fader=null;
+		try {
+			frames=getFrames(pathname);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		lastTime=System.currentTimeMillis();
+		
+	}
+	PGif(String pathname, float x,float y) {
+		super(x,y);
+		fader=null;
+		
+		try {
+			frames=getFrames(pathname);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		lastTime=System.currentTimeMillis();
+	}
+	private Frame[] getFrames(String pathName) throws FileNotFoundException, IOException {
+		GifImage gif= GifDecoder.read(new FileInputStream(pathName));
+		Frame[] answer= new Frame[gif.getFrameCount()];
+		for (int i=0;i<gif.getFrameCount();i++) {
+			answer[i]= new Frame(gif.getFrame(i),gif.getDelay(i));
+		}
+		return answer;
+	}
+	public void draw(PApplet p) {
+		if(fader!=null) {
+			p.pushMatrix();
+			fader.draw(p);
+			p.image(frames[curFrame].getImage(), getX(), getY());
+			p.popMatrix();
+		}else {
+		p.image(frames[curFrame].getImage(), getX(), getY());
+		}
+		advanceFrame();
+		
+	}
+	private void advanceFrame() {
+		long curTime= System.currentTimeMillis();
+		//.getDelay return time in 100ths of a second
+		if(curTime>=lastTime+frames[curFrame].getDelay()*10) {
+			lastTime=curTime;
+			curFrame++;
+			if(curFrame>=frames.length) {
+				curFrame=0;
+			}
+		}
+	}
+	@Override
+	public void resize(int w, int h) {
+		for(int i=0; i< frames.length;i++) {
+			frames[i].getImage().resize(w, h);
+		}
+	}
+	@Override
+	public float getWidth() {
+		return frames[curFrame].getImage().width;
+	}
+	@Override
+	public float getHeight() {
+		return frames[curFrame].getImage().height;
+	}
+	@Override
+	public void fadeOut() {
+		fader=new Fader(255,0,0.2f);
+		fader.start();
+	}
+	@Override
+	public void fadeIn() {
+		fader=new Fader(0,255,0.2f);
+		fader.start();
+	}
+	private class Frame{
+		private final PImage image;
+		private final int delay;
+		public Frame(BufferedImage image, int delay) {
+			this.image= new PImage(image);
+			this.delay=delay;
+		}
+		public PImage getImage() {
+			return image;
+		}
+		public int getDelay() {
+			return delay;
+		}
+	}
+}
---
+++ Textures/src/main/java/Textures/Texture.java
@@ -0,0 +1,153 @@
+/*
+MIT License
+
+Copyright (c) 2020 Kumar.s.Chandra
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+package Textures;
+import processing.core.PApplet;
+// import processing.core.PApplet;
+/**
+ * Represents an image that can be moved around and resized. Supports Gif, jpeg, and png images.
+ * Gifs will be properly animated. Uses processing to draw.
+ * @author Kumar Chandra
+ *
+ */
+public abstract class Texture{
+	private float x,y;
+	/**
+	 * Used by subclasses to create a new Texture
+	 * @param x the x coordinate of the texture
+	 * @param y the y coordinate of the texture
+	 */
+	protected Texture(float x, float y) {
+		this.x=x;
+		this.y=y;
+	}
+	/**
+	 * Resizes the textures image to the given width and height
+	 * @param w new width of texture
+	 * @param h new height of texture
+	 */
+	public abstract void resize(int w, int h);
+	/**
+	 * Returns the current width of the image
+	 * @return
+	 */
+	public abstract float getWidth();
+	/**
+	 * Returns the current height of the image
+	 * @return
+	 */
+	public abstract float getHeight();
+	/**
+	 * Draws the texture onto the given PApplet
+	 * @param p The given PApplet to be drawn to
+	 */
+	public abstract void draw(PApplet p);
+	/**
+	 * Returns the current x coordinate of the texture
+	 * @return current x coordinate
+	 */
+	public float getX() {
+		return x;
+	}
+	/**
+	 * Sets the current x coordinate to the given x coordinate
+	 * @param x The new x coordinate
+	 */
+	public void setX(float x) {
+		this.x=x;
+	}
+	/**
+	 * Returns the current y coordinate of the texture
+	 * @return current y coordinate
+	 */
+	public float getY() {
+		return y;
+	}
+	/**
+	 * Sets the current y coordinate to the given y coordinate
+	 * @param y The new y coordinate
+	 */
+	public void setY(float y) {
+		this.y = y;
+	}
+	/**
+	 * Shifts the current texture a given amount. Positive values shift to the right,
+	 * negative values shift to the left 
+	 * @param shiftX the amount that the shape will be shifted in the x direction
+	 */
+	public void shiftX(float shiftX) {
+		x+=shiftX;
+	}
+	/**
+	 * Shifts the current texture a given amount. Positive values shift to the right,
+	 * negative values shift to the left 
+	 * @param shiftY the amount that the shape will be shifted in the x direction
+	 */
+	public void shiftY(float shiftY) {
+		y+=shiftY;
+	}
+	/**
+	 * Fades the image until it is transparent
+	 */
+	public abstract void fadeOut();
+	/**
+	 * Fades the image from transparent to opaque
+	 */
+	public abstract void fadeIn();
+	/**
+	 * Used to create a Texture given its pathname
+	 * Supports gifs, jpegs, and pngs
+	 * @author Kumar Chandra
+	 *
+	 */
+	public static class TextureBuilder{
+		/**
+		 * Creates a Texture from the image at the given path name
+		 * @param pathName The path to the Image
+		 * @return A texture at 0,0 with the image at the pathname
+		 */
+		public static Texture getTexture(String pathName) {
+			if(pathName.substring(pathName.length()-4, pathName.length()).equals(".gif")){
+				return new PGif(pathName);
+			}else {
+				return new TextureImage(pathName);
+			}
+		}
+		/**
+		 * Creates a Texture from the image at the given path name
+		 * @param pathName The path to the Image
+		 * @param x The starting x coordinate of the texture
+		 * @param y The starting y coordinate of the texture
+		 * @return A texture at x,y with the image at the pathname
+		 */
+		public static Texture getTexture(String pathName,float x, float y) {
+			
+			if(pathName.substring(pathName.length()-4, pathName.length()).equals(".gif")){
+				return new PGif(pathName,x,y);
+			}else {
+				return new TextureImage(pathName,x,y);
+			}
+		}
+	}
+	
+}
